42ee0a0f52a4d0aac328a659aff3a3c80d83521a
Melikov <melikvov@mail.ru>
2026-03-12 17:16:53 +0300

    search_fix

--- wiki-backend/app/api/v1/search.py
+++ wiki-backend/app/api/v1/search.py
@@ -1,32 +1,28 @@
+# app/api/v1/endpoints/search.py
 from fastapi import APIRouter, Depends
-from fastapi_cache.decorator import cache
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.core.database import get_db
 from app.core.config import settings
+from app.core.database import get_db
 from app.schemas.search import SearchQueryParams, SearchResponse, SearchResultItem
-from app.services.search_service import SearchService
+from app.services.search_services.search_service_factory import SearchServiceFactory
+
 router = APIRouter()
 
 @router.get("/", response_model=SearchResponse)
-@cache(expire=settings.cache_expire) 
 async def search_articles(
     params: SearchQueryParams = Depends(),
     db: AsyncSession = Depends(get_db)
 ):
-    """
-    Полнотекстовый поиск по статьям с учётом языка.
-    Использует pg_trgm для нечёткого поиска по заголовку и tsvector для содержания.
-    """
-    service = SearchService(db)
-    total, results = await service.search(
+    # Создаём нужный сервис внутри функции
+    search_service = SearchServiceFactory.create_service(settings.SEARCH_ENGINE, db)
+    total, results = await search_service.search(
         q=params.q,
         language=params.language,
         fields=params.fields,
         limit=params.limit,
         offset=params.offset
     )
-
     return SearchResponse(
         query=params.q,
         language=params.language,
